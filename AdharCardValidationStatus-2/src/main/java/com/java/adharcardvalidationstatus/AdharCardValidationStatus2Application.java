e3c509dee063f8b4bd7c8c8406c1dc60b64badb5
Shilpa <siksun19>
2023-07-28 21:37:55 +0530

    Project added

---
+++ AdharCardValidationStatus-2/src/main/java/com/java/adharcardvalidationstatus/AdharCardValidationStatus2Application.java
@@ -0,0 +1,13 @@
+package com.java.adharcardvalidationstatus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdharCardValidationStatus2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdharCardValidationStatus2Application.class, args);
+	}
+
+}
---
+++ AdharCardValidationStatus-2/src/test/java/com/java/adharcardvalidationstatus/AdharCardValidationStatus2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.adharcardvalidationstatus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdharCardValidationStatus2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
